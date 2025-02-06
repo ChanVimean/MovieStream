3eefe526dc11638c44a50eabb84ae9f60b843bce
ChanVimean <mrrmeanvlogger@gmail.com>
2025-02-06 12:33:38 +0700

    Adding New Releases, Top Anime, Top Donghua, Animation

--- index.js
+++ index.js
@@ -31,11 +31,23 @@ const InitializeApp = async () => {
     Render('trending', Trending(movieList, "Trending", 15))
 
     // New Releases
+    const newRelease = FilterItems(movieList, 'release', "2020")
+    Render('newReleases', Carousel(newRelease, "New Releases"))
+
     // Continue
     // Up Coming
+
     // Top Anime
+    const topAnime = FilterItems(movieList, 'type', ["Anime"])
+    Render('topAnime', Carousel(topAnime, "Top Anime"))
+
     // Top Donghua
+    const topDonhua = FilterItems(movieList, 'type', ["Donghua"])
+    Render('topDonghua', Carousel(topDonhua, "Top Donhua"))
+
     // Animation
+    const animation = FilterItems(movieList, 'type', ["Animation"])
+    Render('animation', Carousel(animation, "Animation"))
 
     // Action
     const genreAction = FilterItems(movieList, 'genre', ['Action'])
